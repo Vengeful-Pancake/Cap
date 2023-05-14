929a9b01739d32beb6b6c507446807d7f231a6ae
Vengeful-Pancake <thinh2000tiger@gmail.com>
2023-05-14 21:06:16 +0700

    "nothing"

--- dhandler/d_scanner.py
+++ dhandler/d_scanner.py
@@ -1,5 +1,96 @@
-import os
-from system_files.initial import *
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
+from PIL import Image
+import torch
+from transformers import GLPNImageProcessor, GLPNForDepthEstimation
+import numpy as np
+import open3d as o3d
 
-def d_scan():
-    pass
+def display_inlier_outlier(cloud, ind):
+    inlier_cloud = cloud.select_by_index(ind)
+    outlier_cloud = cloud.select_by_index(ind, invert=True)
+
+    print("Showing outliers (red) and inliers (gray): ")
+    outlier_cloud.paint_uniform_color([1, 0, 0])
+    inlier_cloud.paint_uniform_color([0.8, 0.8, 0.8])
+    o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud],
+                                      zoom=0.3412,
+                                      front=[0.4257, -0.2125, -0.8795],
+                                      lookat=[2.6172, 2.0475, 1.532],
+                                      up=[-0.0694, -0.9768, 0.2024])
+
+
+feature_extractor = GLPNImageProcessor.from_pretrained("vinvino02/glpn-nyu")
+model = GLPNForDepthEstimation.from_pretrained("vinvino02/glpn-nyu")
+
+# load and resize the input image
+image = Image.open("dhandler\\image.jpg")
+new_height = 480 if image.height > 480 else image.height
+new_height -= (new_height % 32)
+new_width = int(new_height * image.width / image.height)
+diff = new_width % 32
+new_width = new_width - diff if diff < 16 else new_width + 32 - diff
+new_size = (new_width, new_height)
+image = image.resize(new_size)
+
+# prepare image for the model
+inputs = feature_extractor(images=image, return_tensors="pt")
+
+# get the prediction from the model
+with torch.no_grad():
+    outputs = model(**inputs)
+    predicted_depth = outputs.predicted_depth
+
+# remove borders
+pad = 16
+output = predicted_depth.squeeze().cpu().numpy() * 1000.0
+output = output[pad:-pad, pad:-pad]
+image = image.crop((pad, pad, image.width - pad, image.height - pad))
+
+# visualize the prediction
+fig, ax = plt.subplots(1, 2)
+ax[0].imshow(image)
+ax[0].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+ax[1].imshow(output, cmap='plasma')
+ax[1].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+plt.tight_layout()
+plt.savefig('my_plot.png')
+
+width, height = image.size
+
+depth_image = (output * 255 / np.max(output)).astype('uint8')
+image = np.array(image)
+
+# create rgbd image
+depth_o3d = o3d.geometry.Image(depth_image)
+image_o3d = o3d.geometry.Image(image)
+rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(image_o3d, depth_o3d, convert_rgb_to_intensity=False)
+
+# camera settings
+camera_intrinsic = o3d.camera.PinholeCameraIntrinsic()
+camera_intrinsic.set_intrinsics(width, height, 500, 500, width/2, height/2)
+
+# create point cloud
+pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, camera_intrinsic)
+
+# outliers removal
+cl, ind = pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=20.0)
+pcd = pcd.select_by_index(ind)
+
+# estimate normals
+pcd.estimate_normals()
+pcd.orient_normals_to_align_with_direction()
+
+# surface reconstruction
+mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=10, n_threads=1)[0]
+
+# rotate the mesh
+rotation = mesh.get_rotation_matrix_from_xyz((np.pi, 0, 0))
+mesh.rotate(rotation, center=(0, 0, 0))
+
+# save the mesh
+o3d.io.write_triangle_mesh(f'./mesh.obj', mesh)
+
+# visualize the mesh
+o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
